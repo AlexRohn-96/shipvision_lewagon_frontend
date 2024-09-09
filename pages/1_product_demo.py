c57c6355230c1ddda9049c7a39445d01ef814e13
Alexander Rohn <alexander.rohn@hec.edu>
2024-09-09 13:00:56 +0200

    Small change

--- pages/1_product_demo.py
+++ pages/1_product_demo.py
@@ -42,13 +42,10 @@ if uploaded_file is not None:
         pixel_values = red_channel + green_channel + blue_channel
 
 
-
-
-
         #pixel_values = [0, 128, 255]
         #breakpoint()
         # Define your API endpoint
-        # api_url = "http://127.0.0.1:8000/predict"  # Replace with your actual API URL
+        #api_url = "http://127.0.0.1:8000/predict"  # Replace with your actual API URL
         api_url = "https://shipvision-647806685234.europe-west1.run.app/predict"  # Replace with your actual API URL
         #https://shipvision-647806685234.europe-west1.run.app/
         # Send the list of pixel values to the API as JSON (POST request)
@@ -59,7 +56,8 @@ if uploaded_file is not None:
                 api_url,
                 json={"X": pixel_values}  # Send the normalized pixel list as JSON
             )
-            #breakpoint()
+
+
 
             # Check if the request was successful
             if response.status_code == 200:
